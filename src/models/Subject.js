f261736928b3feda19259485f95b71ce21b0faf3
tamastotok <tamastotok.tk@gmail.com>
2026-03-04 02:27:53 +0100

    model simplified

--- src/models/Subject.js
+++ src/models/Subject.js
@@ -7,23 +7,33 @@ const SubjectSchema = new mongoose.Schema(
       required: [true, 'A tárgy neve kötelező'],
       trim: true,
     },
-    description: {
-      type: String,
-      trim: true,
-    },
     requirement: {
       type: String,
+      enum: {
+        values: ['Beadandó feladat', 'Zárthelyi dolgozat', 'Szóbeli vizsga'],
+        message: '{VALUE} nem érvényes követelmény típus!',
+      },
+      required: [true, 'A követelmény típusát kötelező kiválasztani'],
+    },
+    taskTitle: {
+      type: String,
+      required: [true, 'A feladat címe kötelező'],
       trim: true,
+    },
+    deadline: {
+      type: Date,
+      required: [true, 'A határidő megadása kötelező'],
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ['hátralévő', 'folyamatban', 'befejezett'],
+        message: '{VALUE} nem érvényes státusz!',
+      },
+      default: 'hátralévő',
     },
   },
   { timestamps: true },
-  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
 );
-
-SubjectSchema.virtual('tasks', {
-  ref: 'Task',
-  localField: '_id',
-  foreignField: 'subject',
-});
 
 module.exports = mongoose.model('Subject', SubjectSchema);
